425e854f2fd81337f7000680bf2b751d6b7f8b5b
Grigory Petrov <grigory.v.p@gmail.com>
2015-05-14 09:16:36 +0300

    Auto generated app.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='djangosprint',
+    version='1.0',
+    description='djangosprint.cf website',
+    author='Grigory Petrov',
+    author_email='grigory.v.p@gmail.com',
+    url='http://djangosprint.cf/',
+    install_requires=['Django==1.8.1'],
+)
+
