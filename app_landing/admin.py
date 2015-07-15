a2d3c9c6a066668ce0aaaede311f7e901cbcbae9
Grigory Petrov <grigory.v.p@gmail.com>
2015-07-15 07:42:19 +0300

    models for all blocks

--- app_landing/admin.py
+++ app_landing/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 import app_landing.models
 
 class BlockAdmin(admin.ModelAdmin):
-    list_display = ('type', 'title')
+    list_display = ('order', 'type', 'title')
 
 admin.site.register(app_landing.models.Block, BlockAdmin)
 
--- app_landing/models.py
+++ app_landing/models.py
@@ -6,11 +6,18 @@ class Block(models.Model):
         NONE = 'none'
         ABOUT = 'about'
         SCHEDULE = 'schedule'
+        ORGANIZERS = 'organizers'
+        SPONSORS = 'sponsors'
+        MAP = 'map'
 
     type = models.CharField(default = Type.NONE, max_length = 50, choices = (
         (Type.NONE, "none"),
         (Type.ABOUT, "about"),
         (Type.SCHEDULE, "schedule"),
+        (Type.ORGANIZERS, "organizers"),
+        (Type.SPONSORS, "sponsors"),
+        (Type.MAP, "map"),
     ))
-    title = models.CharField(max_length = 100)
+    title = models.CharField(default="", max_length = 100)
+    order = models.IntegerField(default = 0)
 
